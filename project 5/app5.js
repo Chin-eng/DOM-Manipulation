9189a58e712da19eab330f63ce524ae79d0a4780
Chin-Erdene Gantulga <chin-erdenegantulga@Chin-Erdenes-MacBook-Air.local>
2022-10-27 12:50:52 +0800

    checkButton added

--- project 5/app5.js
+++ project 5/app5.js
@@ -1,35 +1,105 @@
+myList = []
+
 const addTask = document.getElementById('add-task');
 const taskContainer = document.getElementById('task-container');
 const inputTask = document.getElementById('input-task');
 
 
+
 //event listerer for add button
 
-addTask.addEventListener('click', function() {
+// addTask.addEventListener('click', function() {
 
-    let task = document.createElement('div')
-    task.classList.add('task')
-    console.log(task)
+//     let task = document.createElement('div')
+//     task.classList.add('task')
+    
 
-    let li = document.createElement('li')
-    li.innerText = `${inputTask.value}`
-    console.log(li);
-    task.appendChild(li);
+//     let li = document.createElement('li')
+//     li.innerText = `${inputTask.value}`
+  
+//     task.appendChild(li);
 
 
-    let checkButton = document.createElement("button")
-    checkButton.innerHTML = '<i class = "fa-solid fa-check">'
-    checkButton.classList.add('checkTask')
-    task.appendChild(checkButton)
+//     let checkButton = document.createElement("button")
+//     checkButton.innerHTML = '<i class = "fa-solid fa-check">'
+//     checkButton.classList.add('checkTask')
+//     task.appendChild(checkButton)
 
-    let deleteButton = document.createElement("button")
-    deleteButton.innerHTML = '<i class = "fa-solid fa-trash-can">'
-    deleteButton.classList.add('deleteTask')
-    task.appendChild(deleteButton)
+//     let deleteButton = document.createElement("button")
+//     deleteButton.innerHTML = '<i class = "fa-solid fa-trash-can">'
+//     deleteButton.classList.add('deleteTask')
+//     task.appendChild(deleteButton)
 
-    if (inputTask.value === " ") {
-        alert('Please Enter a Task')
-    } else {
-        taskContainer.appendChild(task);
+//     if (inputTask.value === " ") {
+//         alert('Please Enter a Task')
+//     } else {
+//         taskContainer.appendChild(task);
+//     }
+
+//     inputTask.value = "";
+
+//     checkButton.addEventListener('click', function() {
+//         checkButton.parentElement.style.textDecoration = "line-through"
+//     })
+
+//     deleteButton.addEventListener('click', function(e) {
+//         let target = e.target;
+        
+//         console.log(target.parentElement)
+
+//         target.parentElement.remove()
+{/* <i class = "fa-solid fa-check"></i>
+<i class = "fa-solid fa-trash-can"></i> */}
+
+//     })
+
+// })
+
+function render(leads) {
+    let listItems = " "
+    for (let i = 0; i < leads.length; i++) {
+        listItems += 
+            `<li class= "task"> 
+                ${leads[i]} 
+                <button class="checkTask"> 
+                    <i class = "fa-solid fa-check"></i>
+                <button class = "deleteTask"> 
+                    <i class = "fa-solid fa-trash-can"></i>
+            </li>`
     }
+    taskContainer.innerHTML = listItems
+}
+
+
+addTask.addEventListener('click', function() {
+    myList.push(inputTask.value)
+    inputTask.value = ""
+    render(myList)
+
+
+    const checkButton = document.querySelectorAll(".checkTask");
+    for (i = 0; i < checkButton.length; i++) {
+        checkButton[i].addEventListener('click', function() {
+            this.parentElement.style.textDecoration = "line-through"
+        }) 
+    }
+    
+    
+    
+    // checkButton.addEventListener('click', function() {
+    //     checkButton.parentElement.style.textDecoration = "line-through"
+    // })
+
+    // const deleteButton = document.querySelector(".deleteTask");
+    // deleteButton.addEventListener('click', function(e) {
+
+    // }) 
 })
+
+
+
+
+
+
+
+
